653fce7a2301cf3d01eac42b3b31d4f7bb1512a8
JacenWilliams <jwilli06@g.uafs.edu>
2018-11-27 23:05:47 -0600

    Added missed files

---
+++ src/Word.java
@@ -0,0 +1,12 @@
+
+public class Word {
+	public String value;
+	public int index;
+	public int count;
+	
+	public Word(String value, int index) {
+		this.value = value;
+		this.index = index;
+		this.count = 1;
+	}
+}
